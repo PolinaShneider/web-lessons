fde70f380e48d40d043db821edc7c5d8c456e2a9
PolinaShneider <beatrixlinkoln@gmail.com>
2019-04-08 00:34:24 +0300

    Timer tasks

--- test.js
+++ test.js
@@ -1,7 +1,19 @@
 describe("pow", function() {
 
-  it("возводит в n-ю степень", function() {
-    assert.equal(pow(2, 3), 8);
+  describe("возводит x в степень n", function() {
+
+    function makeTest(x) {
+      var expected = x * x * x;
+      it("при возведении " + x + " в степень 3 результат: " + expected, function() {
+        assert.equal(pow(x, 3), expected);
+      });
+    }
+
+    for (var x = 1; x <= 5; x++) {
+      makeTest(x);
+    }
+
   });
 
-}); 
+  // ... дальнейшие тесты it и подблоки describe ...
+});
